972e67dd52f17224b7a49f9c8e1866d0e7fbc08e
malkov-am <am.malkov91@gmail.com>
2022-09-08 16:54:15 +0300

    Add section Techs

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.styles.scss";
 import Hero from "../Hero/Hero.component";
 import AboutProject from "../AboutProject/AboutProject.component";
+import Techs from "../Techs/Techs.component";
 
 const App = () => {
   return (
     <div className='App'>
       <Hero />
       <AboutProject />
+      <Techs />
     </div>
   );
 };
