f7ca83a0c070b4e8a2402c8c695af06902692ab5
Andrei Malkov <thewildcat@yandex.ru>
2022-09-12 22:32:51 +0300

    Complete Login page

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import './Login.styles.scss';
-import SignForm from '../SignForm/SignForm.component';
+import React from "react";
+import "./Login.styles.scss";
+import Button, {
+  BUTTON_COLOR_CLASSES,
+  BUTTON_TYPE_CLASSES,
+} from "../Button/Button.component";
+import SignForm from "../SignForm/SignForm.component";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <>
-            <SignForm title='Рады видеть!' buttonTitle='Войти'>
-              <label className='sign__form-label'>E-mail
-                <input name='email' className='sign__form-input' type='text' />
-              </label>
-              <label className='sign__form-label'>Пароль
-                <input name='password' className='sign__form-input' type='text' />
-              </label>
-            </SignForm>
-        </>
-    );
+  return (
+    <SignForm title='Рады видеть!'>
+      <div>
+        <label for='email' className='sign__form-label'>
+          E-mail
+        </label>
+        <input name='email' className='sign__form-input' type='text' />
+        <p className='sign__form-err-message'>Что-то пошло не так...</p>
+        <label for='password' className='sign__form-label'>
+          Пароль
+        </label>
+        <input name='password' className='sign__form-input' type='password' />
+        <p className='sign__form-err-message'>Что-то пошло не так...</p>
+      </div>
+      <div className='sign__form-buttons'>
+        <Button
+          buttonType={BUTTON_TYPE_CLASSES.sizeL}
+          color={BUTTON_COLOR_CLASSES.white}
+        >
+          Войти
+        </Button>
+        <p className='sign__form-invite'>
+          Ещё не зарегистрированы?
+          <Link className='sign__form-link' to='/signup'>
+            Регистрация
+          </Link>
+        </p>
+      </div>
+    </SignForm>
+  );
 };
 
 export default Login;
--- src/components/SignForm/SignForm.component.js
+++ src/components/SignForm/SignForm.component.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './SignForm.styles.scss';
-import Button, {
-    BUTTON_COLOR_CLASSES,
-    BUTTON_TYPE_CLASSES,
-  } from "../Button/Button.component";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./SignForm.styles.scss";
+import Button, { BUTTON_TYPE_CLASSES } from "../Button/Button.component";
+import { useNavigate } from "react-router-dom";
 
-const SignForm = ({ title, buttonTitle, children }) => {
-    const navigate = useNavigate();
+const SignForm = ({ title, children }) => {
+  const navigate = useNavigate();
 
-    return (
-        <div className='sign'>
-            <Button buttonType={BUTTON_TYPE_CLASSES.home} onClick={() => navigate('/')} />
-            <h2 className='sign__title'>{title}</h2>
-            <form className='sign__form' noValidate>
-            {children}
-            <Button buttonType={BUTTON_TYPE_CLASSES.sizeL} color={BUTTON_COLOR_CLASSES.white}>{buttonTitle}</Button>
-            </form>
-        </div>
-    );
+  return (
+    <div className='sign'>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.home}
+        onClick={() => navigate("/")}
+      />
+      <h2 className='sign__title'>{title}</h2>
+      <form className='sign__form' noValidate>
+        {children}
+      </form>
+    </div>
+  );
 };
 
 export default SignForm;
