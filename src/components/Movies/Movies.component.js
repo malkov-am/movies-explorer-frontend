55e06e5240436168eadb25e09203166c4e625f0a
malkov-am <am.malkov91@gmail.com>
2022-09-12 14:16:05 +0300

    Add Section Movies

--- src/components/Movies/Movies.component.js
+++ src/components/Movies/Movies.component.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './Movies.styles.scss';
 import SearchForm from "../SearchForm/SearchForm.component";
+import MoviesCardList from "../MoviesCardList/MoviesCardList.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../Button/Button.component";
 
 const Movies = () => {
   return <div className="movies">
     <SearchForm />
+    <MoviesCardList />
+    <Button buttonType={BUTTON_TYPE_CLASSES.more}>Еще</Button>
   </div>;
 };
 
--- src/components/MoviesCardList/MoviesCardList.component.js
+++ src/components/MoviesCardList/MoviesCardList.component.js
@@ -1,11 +1,54 @@
 import React from "react";
+import './MoviesCardList.styles.scss';
 import MoviesCard from "../MoviesCard/MoviesCard.component";
 import imgPath from "../../images/card/card-no-img.jpg";
 
 const MoviesCardList = () => {
   return (
-    <div>
+    <div className="movies-card-list">
       <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
+        card={{
+          nameRU: "33 слова о дизайне",
+          duration: "1ч 47м",
+          image: imgPath,
+        }}
+      />
+            <MoviesCard
         card={{
           nameRU: "33 слова о дизайне",
           duration: "1ч 47м",
