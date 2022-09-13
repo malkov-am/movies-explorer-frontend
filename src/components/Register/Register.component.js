f2d1f2c9ab39af69e185fb4b6422856f62e73f54
malkov-am <am.malkov91@gmail.com>
2022-09-13 08:51:13 +0300

    Add Register Component

--- src/components/Register/Register.component.js
+++ src/components/Register/Register.component.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import './Register.styles.scss';
+import Button, {
+    BUTTON_COLOR_CLASSES,
+    BUTTON_TYPE_CLASSES,
+  } from "../Button/Button.component";
+  import SignForm from "../SignForm/SignForm.component";
+  import { Link } from "react-router-dom";
 
 const Register = () => {
     return (
+        <SignForm title='Добро пожаловать!'>
         <div>
-            
+        <label for='name' className='sign__form-label'>
+            Имя
+          </label>
+          <input name='name' className='sign__form-input' type='text' />
+          <p className='sign__form-err-message'>Что-то пошло не так...</p>
+          <label for='email' className='sign__form-label'>
+            E-mail
+          </label>
+          <input name='email' className='sign__form-input' type='text' />
+          <p className='sign__form-err-message'>Что-то пошло не так...</p>
+          <label for='password' className='sign__form-label'>
+            Пароль
+          </label>
+          <input name='password' className='sign__form-input' type='password' />
+          <p className='sign__form-err-message'>Что-то пошло не так...</p>
         </div>
+        <div className='sign__form-buttons'>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.sizeL}
+            color={BUTTON_COLOR_CLASSES.white}
+          >
+            Войти
+          </Button>
+          <p className='sign__form-invite'>
+            Уже зарегистрированы?
+            <Link className='sign__form-link' to='/signin'>
+              Войти
+            </Link>
+          </p>
+        </div>
+      </SignForm>
     );
 };
 
